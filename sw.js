b9752620c4165a32df4df2a9cf1a7830f9da4927
mustakasICS <77923579+mustakasICS@users.noreply.github.com>
2021-01-24 20:31:21 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -8,13 +8,6 @@ function handleOrientation(event) {
 self.addEventListener('deviceorientation', handleOrientation);
 
 self.addEventListener('fetch', function (event) {
-  function handleOrientation(event) {
-    var x = event.beta;  // In degree in the range [-180,180)
-    var y = event.gamma; // In degree in the range [-90,90)
-    console.log(x);
-    console.log(y);
-  }
-  self.addEventListener('deviceorientation', handleOrientation);
   console.log(event.request.url);
 });
 console.log("OTI NA NAI");
