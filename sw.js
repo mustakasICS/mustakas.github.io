4f44b22ff26acde261b52c8ee874183537fb1610
mustakasICS <77923579+mustakasICS@users.noreply.github.com>
2021-01-24 14:55:58 +0200

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,10 @@
+self.addEventListener('fetch', function (event) {
+  function handleOrientation(event) {
+    var x = event.beta;  // In degree in the range [-180,180)
+    var y = event.gamma; // In degree in the range [-90,90)
+    console.log(x);
+    console.log(y);
+  }
+  window.addEventListener('deviceorientation', handleOrientation);
+  console.log(event.request.url);
+});
