3abddd57da161c83d677c3817ddc8c9a0f3310e3
mustakasICS <77923579+mustakasICS@users.noreply.github.com>
2021-01-25 21:58:37 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,12 +1,3 @@
-const sendMessageToPage = function (message) {
-    return self.clients.matchAll().then(clients => {
-        return Promise.all(clients.map(client => {
-            return client.postMessage('(service worker message) ' + message);
-        }));
-    });
-};
-
-
 function handleOrientation(event) {
     var x = event.beta;  // In degree in the range [-180,180)
     var y = event.gamma; // In degree in the range [-90,90)
