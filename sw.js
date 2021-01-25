bb79cb225cdae48eb30bdafb650b182e8d973efd
mustakasICS <77923579+mustakasICS@users.noreply.github.com>
2021-01-25 22:45:20 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -36,3 +36,16 @@ clients.matchAll({
 
     return clientIsVisible;
   });
+
+    let acl = new Accelerometer({frequency: 30});
+    let max_magnitude = 0;
+    acl.addEventListener('activate', () => console.log('Ready to measure.'));
+    acl.addEventListener('error', error => console.log(`Error: ${error.name}`));
+    acl.addEventListener('reading', () => {
+        let magnitude = Math.hypot(acl.x, acl.y, acl.z);
+        if (magnitude > max_magnitude) {
+            max_magnitude = magnitude;
+            console.log(`Max magnitude: ${max_magnitude} m/s2`);
+        }
+    });
+    acl.start();
