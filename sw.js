c2a21be71939954d9ff6c038df8def9b17934016
mustakasICS <77923579+mustakasICS@users.noreply.github.com>
2021-01-25 22:24:21 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -4,9 +4,35 @@ function handleOrientation(event) {
     console.log(x);
     console.log(y);
   }
-self.window.addEventListener('deviceorientation', handleOrientation);
+self.addEventListener('deviceorientation', handleOrientation);
 
 self.addEventListener('fetch', function (event) {
   console.log(event.request.url);
 });
 console.log("OTI NA NAI");
+
+clients.matchAll({
+       type: 'window',
+       includeUncontrolled: true
+    })
+    .then(function(windowClients) {
+
+     var clientIsVisible = false;
+
+     for (var i = 0; i < windowClients.length; i++) {
+      const windowClient = windowClients[i];
+      windowClient.addEventListener('deviceorientation', function (event) {
+        var x = event.beta;  // In degree in the range [-180,180)
+        var y = event.gamma; // In degree in the range [-90,90)
+        console.log(x);
+        console.log(y)
+      });
+      if (windowClient.visibilityState==="visible") {
+          clientIsVisible = true;
+    
+        break;
+      }
+    }
+
+    return clientIsVisible;
+  });
